68b23dba64407cf4ba16fd6d6625d576123016d2
이현기 <lhgwoo2@naver.com>
2022-12-15 23:14:49 +0900

    [기능] 알람 액티비티 추가 및 메인 액티비티 패키지 이동

---
+++ app/src/main/java/com/loveon/getonup/ui/AlarmActivity.kt
@@ -0,0 +1,14 @@
+package com.loveon.getonup.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.loveon.getonup.R
+
+class AlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+
+
+}
